b6d8102d00a589d399f5e3b8066084c04c0935c3
shal0m-bon <151454475+shal0m-bon@users.noreply.github.com>
2024-08-21 09:13:02 +0100

    Add files via upload
    Created a button which when clicked, converts an inputed number from celsius to farenheit

---
+++ Celsius_converter/assets/script.js
@@ -0,0 +1,15 @@
+//get input
+//store in variable
+//calculate the farenheit equivalence
+//display the result on screen
+
+
+// const input = document.getelementbyid('number').value
+// const ans = document.getElementById('ans').value
+
+
+// function celsius(){
+//     let x = document.getElementById('number').value
+//     let y = (x - 32) * 5/9;
+//     console.log(y)
+// }
